1c523f14372aa2d027c045f9c6e323f4d9df6747
Alex Tsirel <noma4i@gmail.com>
2020-08-29 19:41:09 +1000

    initial test suite

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,22 @@ Bundler.require(:default)
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'sqlite3'
+require 'sidekiq/testing'
+
+module SidekiqMinitestSupport
+  def after_teardown
+    Sidekiq::Worker.clear_all
+    super
+  end
+end
+
+class MiniTest::Spec
+  include SidekiqMinitestSupport
+end
+
+class MiniTest::Unit::TestCase
+  include SidekiqMinitestSupport
+end
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
 PaperTrail.config.mongo_config = { hosts: ['localhost:27017'], database: 'my_test_db' }
