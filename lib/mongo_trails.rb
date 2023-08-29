3a472371382dc56812e9965a9edd25c19aa2034b
Daryl Moreno <dmoreno@hsi.com>
2023-08-29 12:36:05 -0700

    RecordTrail class properly loaded after original PaperTrail#RecordTrail
    * Fix: required 'paper_trail/record_trail' before 'mongo_trails/record_trail'
    * Fix: check to save version before saving version deleted

--- lib/mongo_trails.rb
+++ lib/mongo_trails.rb
@@ -3,6 +3,7 @@
 require 'paper_trail'
 require 'paper_trail/version_concern'
 require 'paper_trail/model_config'
+require 'paper_trail/record_trail'
 
 require 'mongo_trails/config'
 require 'mongo_trails/model_config'
