6ff80882e0aa1cbd30fa48d3db4c73d9af0cafce
Ivan Romaniuk <ivanvl@gmail.com>
2025-02-06 17:12:51 -0500

    remove comments

--- lib/mongo_trails/model_config.rb
+++ lib/mongo_trails/model_config.rb
@@ -14,40 +14,5 @@ module PaperTrail
         end
       RUBY
     end
-  #
-  #   def on_create
-  #     @model_class.after_create_commit do |r|
-  #       r.paper_trail.record_create if r.paper_trail.save_version?
-  #     end
-  #     append_option_uniquely(:on, :create)
-  #   end
-  #
-  #   def on_destroy(recording_order = 'before')
-  #     assert_valid_recording_order_for_on_destroy(recording_order)
-  #     @model_class.after_destroy_commit do |r|
-  #       break unless r.paper_trail.save_version?
-  #       r.paper_trail.record_destroy(recording_order)
-  #     end
-  #     append_option_uniquely(:on, :destroy)
-  #   end
-  #
-  #   def on_update
-  #     @model_class.before_save do |r|
-  #       r.paper_trail.reset_timestamp_attrs_for_update_if_needed
-  #     end
-  #     @model_class.after_update_commit do |r|
-  #       if r.paper_trail.save_version?
-  #         r.paper_trail.record_update(
-  #           force: false,
-  #           in_after_callback: true,
-  #           is_touch: false
-  #         )
-  #       end
-  #     end
-  #     @model_class.after_update_commit do |r|
-  #       r.paper_trail.clear_version_instance
-  #     end
-  #     append_option_uniquely(:on, :update)
-  #   end
   end
 end
